f3c7d044645365c87c29dac3b30f651647bb79bf
mudunuriRaju <mr.kesav@gmail.com>
2016-08-31 17:30:28 +0530

    gulp file added to generate all.js and package.json update for gulp requirments

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,20 @@
  */
 var gulp = require('gulp'),
     bower = require('gulp-bower');
+var minify = require('gulp-minify');
+var concat = require('gulp-concat');
 
-var config = {
-    bowerDir: './public/bower_components'
-}
-
-gulp.task('bower', function() {
-    return bower()
-        .pipe(gulp.dest(config.bowerDir))
+gulp.task('compress', function () {
+    gulp.src('public/view/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(minify({
+            ext: {
+                src: '-debug.js',
+                min: '.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: []
+        }))
+        .pipe(gulp.dest('public/javascripts/'))
 });
-
-$ gulp.task('default', ['bower']);
+gulp.task('default', ['compress']);
