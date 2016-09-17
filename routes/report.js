fa0a6066991a498f5de0c58f07fb380a2008ff56
mudunuriRaju <mr.kesav@gmail.com>
2016-09-17 23:17:14 +0530

    date and  format in report

--- routes/report.js
+++ routes/report.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
+var async = require('async');
+var dateFormat = require('dateformat');
 
 var fs = require('fs');
 var router = express.Router();
@@ -43,16 +45,49 @@ router.get('/', function (req, res, next) {
         if (err) {
             next()
         }
-        var obj = {
-            url: fullUrl,
-            date: today,
-            data: workorders
-        };
+
         if (workorders.length > 0) {
-            var renderedHtml = nunjucks.render('./view/ReportHourBase/report_hour.html', obj);
-            pdf.create(renderedHtml, {ticketnum: 'hello'}).toStream(function (err, stream) {
-                stream.pipe(res);
+            var calls = [];
+            workorders.forEach(function (work) {
+                calls.push(function (callback) {
+                    Category.findOne({_id: work.workorder_category}, function (e, r) {
+                        if (e) {
+                            return false;
+                        }
+                        work.workorder_category = r.category_name;
+                    });
+                    Equipment.findOne({_id: work.workorder_equipment}, function (er, ra) {
+                        if (er) {
+                            return false;
+                        }
+                        work.workorder_equipment = ra.equipment_name;
+                        callback(null, work);
+                    });
+
             });
+
+            });
+
+            async.parallel(calls, function (err, result) {
+
+                /* this code will run after all calls finished the job or
+                 when any of the calls passes an error */
+
+                if (err)
+                    return console.log(err);
+                var obj = {
+                    url: fullUrl,
+                    date: today,
+                    data: result
+                };
+                var renderedHtml = nunjucks.render('./view/ReportHourBase/report_hour.html', obj);
+                pdf.create(renderedHtml, {ticketnum: 'hello'}).toStream(function (err, stream) {
+                    stream.pipe(res);
+                });
+                console.log(result);
+            });
+
+
         } else {
             res.redirect(fullUrl + "/#!/search_report_hour");
         }
@@ -74,10 +109,47 @@ router.get('/report_category', function (req, res, next) {
             data: workorders
         };
         if (workorders.length > 0) {
-            var renderedHtml = nunjucks.render('./view/ReportClosedWorkOrder/report_closed.html', obj);
-            pdf.create(renderedHtml, {ticketnum: 'hello'}).toStream(function (err, stream) {
-                stream.pipe(res);
+            var calls = [];
+            workorders.forEach(function (work) {
+                calls.push(function (callback) {
+                    work.wo_datecomplete = dateFormat(new Date(work.wo_datecomplete).toISOString(), 'isoDate');
+                    Category.findOne({_id: work.workorder_category}, function (e, r) {
+                        if (e) {
+                            return false;
+                        }
+                        work.workorder_category = r.category_name;
+                    });
+                    Equipment.findOne({_id: work.workorder_equipment}, function (er, ra) {
+                        if (er) {
+                            return false;
+                        }
+                        work.workorder_equipment = ra.equipment_name;
+                        callback(null, work);
+                    });
+
+                });
+
             });
+
+            async.parallel(calls, function (err, result) {
+
+                /* this code will run after all calls finished the job or
+                 when any of the calls passes an error */
+
+                if (err)
+                    return console.log(err);
+                var obj = {
+                    url: fullUrl,
+                    date: today,
+                    data: result
+                };
+                var renderedHtml = nunjucks.render('./view/ReportClosedWorkOrder/report_closed.html', obj);
+                pdf.create(renderedHtml, {ticketnum: 'hello'}).toStream(function (err, stream) {
+                    stream.pipe(res);
+                });
+                console.log(result);
+            });
+
         } else {
             res.redirect(fullUrl + "/#!/search_closed_report");
         }
@@ -87,5 +159,6 @@ router.get('/report_category', function (req, res, next) {
 
     // res.render('index', { title: 'Prysmian Group - Maintenance Work Order Application' });
 });
+
 
 module.exports = router;
