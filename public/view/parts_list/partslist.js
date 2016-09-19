0af9fec73ea2f27e6b32ebd4f523cd082e9500f2
mudunuriRaju <mr.kesav@gmail.com>
2016-09-19 17:40:01 +0530

    Part List dislay

--- public/view/parts_list/partslist.js
+++ public/view/parts_list/partslist.js
@@ -9,10 +9,23 @@ angular.module('PGapp.partslist', ['ngRoute','ngAnimate', 'ngCookies'])
   });
 }])
 
-.controller('PlistCtrl', ["$scope","$cookies","$location",'API',function($scope,$cookies,$location,API) {
+    .controller('PlistCtrl', ["$scope", "$cookies", "$location", "$filter", 'API', function ($scope, $cookies, $location, $filter, API) {
   if(!$cookies.get('userDetails')){
     $location.path('login');
   }
+      var userdetail = $cookies.getObject('userDetails');
+      API.Equipments.Recent(userdetail.user, function (res) {
+        if (res.Code == 200) {
+
+          $scope.equipments = res.Info.equipments;
+          //$cookies.put('userDetails',res)
+        } else {
+
+        }
+
+      }, function (error) {
+        alert(error);
+      });
   $scope.Logout = function () {
       $cookies.remove('userDetails');
     $location.path("/");
@@ -20,4 +33,13 @@ angular.module('PGapp.partslist', ['ngRoute','ngAnimate', 'ngCookies'])
   $scope.redirectLoc = function (reloc) {
     $location.path(reloc);
   }
+      $scope.$watch("equipment_number", function (newValue, oldValue) {
+        if (!angular.isUndefined($scope.equipment_number)) {
+          var found = $filter('getByFacilityNumber')('equipment_number', $scope.equipment_number, $scope.equipments);
+          if (angular.isUndefined(found) || found === null) {
+            return null;
+          }
+          $scope.parts = found.equipments;
+        }
+      });
 }]);
