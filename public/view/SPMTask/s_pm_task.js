9dd4db3a3c4778be79601d7d64e5f37bae5e1a9f
mudunuriRaju <mr.kesav@gmail.com>
2016-10-04 18:33:15 +0530

    PM Task report date validation done

--- public/view/SPMTask/s_pm_task.js
+++ public/view/SPMTask/s_pm_task.js
@@ -69,11 +69,13 @@ angular.module('PGapp.searchpmtask', ['ngRoute', 'ngAnimate', 'ngCookies'])
         $scope.$watch("datefrom", function (newValue, oldValue) {
             if (!angular.isUndefined($scope.datefrom)) {
                 $scope.workOrder.wo_datefrom = new Date($scope.datefrom).valueOf();
+                $scope.minDate = new Date($scope.datefrom);
             }
         });
         $scope.$watch("dateto", function (newValue, oldValue) {
             if (!angular.isUndefined($scope.dateto)) {
                 $scope.workOrder.wo_dateto = new Date($scope.dateto).valueOf();
+                $scope.maxDate = new Date($scope.dateto);
             }
         })
 
